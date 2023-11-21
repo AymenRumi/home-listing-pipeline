5b8cf6e0d9a058a058d23b00206f4d472fce2060
Aymen Rumi <aymen.rumi@mail.mcgill.ca>
2023-11-20 21:37:45 -0500

    etl task

--- src/tasks/etl_worker.py
+++ src/tasks/etl_worker.py
@@ -4,4 +4,4 @@ from src.pipe import Extract, Load, Validate
 
 @app.task
 def run_etl_worker(pages: list):
-    Load().push_to_db(Validate().run_data_contract(Extract().get_listing_data()))
+    Load().push_to_db(Validate().run_data_contract(Extract().get_listing_data(pages)))
