0f4f9cc62b5ba20ceb5188f4099653bb33416f5b
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-08 12:50:00 -0500

    distribute workload

--- remax_pipeline/pipeline.py
+++ remax_pipeline/pipeline.py
@@ -2,13 +2,13 @@ from .celery_app import run_etl_worker
 from .pipe import Extract
 
 
-def run(dev: bool = False):
+def run(with_celery: bool = True):
 
     workload = Extract.get_workload()
 
-    action = {True: run_etl_worker, False: run_etl_worker.delay}
+    action = {False: run_etl_worker, True: run_etl_worker.delay}
 
-    return [action[dev](pages) for pages in workload]
+    return [action[with_celery](pages) for pages in workload]
 
 
 def run_local(dev: bool = False):
