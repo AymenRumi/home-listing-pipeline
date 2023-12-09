a428f2df1f78f7342900e9890937b85449181cf0
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-09 18:38:02 -0500

    package upload

--- remax_pipeline/tasks/etl_task.py
+++ remax_pipeline/tasks/etl_task.py
@@ -3,7 +3,7 @@ import time
 from ..pipe import Extract, Load, Validate
 
 
-def start_worker(pages: list):
+def start_task(pages: list):
     worker_start_time = time.time()
     return Load.push_to_db(
         Validate.data_contract(Extract.get_listing_data(pages=pages, multithreaded=True)), worker_start_time
