fa7f4b4f04706876cba20480281acaceb3e8730c
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-09 18:42:04 -0500

    package version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AymenRumi/remax-data-pipeline",  # Your project's homepage
     packages=find_packages(),
-    version="0.1.14",
+    version="0.1.15",
     # cmdclass=versioneer.get_cmdclass(),
     classifiers=[
         "Programming Language :: Python :: 3",
