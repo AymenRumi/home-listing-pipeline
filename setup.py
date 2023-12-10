b37bc6da368ea39ba625195d29c063685ee52a2c
AymenRumi <aymen.rumi@mail.mcgill.ca>
2023-12-09 21:01:33 -0500

    init file for decorator

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AymenRumi/remax-data-pipeline",  # Your project's homepage
     packages=find_packages(),
-    version="0.2.2",
+    version="0.2.3",
     # cmdclass=versioneer.get_cmdclass(),
     classifiers=[
         "Programming Language :: Python :: 3",
